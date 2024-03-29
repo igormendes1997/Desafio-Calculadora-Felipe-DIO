72d52a994c09fd8a18d505fd2867f009bce6ee3d
igormendes1997 <igor19_@outlook.com>
2024-03-29 02:53:14 -0300

    organizei a linhas de codigo

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-function calcularRanking(vitorias, derrotas) {
-         let saldo = vitorias - derrotas
-         let nivel = "" ;
+    function calcularRanking(vitorias, derrotas) {
 
-         if (vitorias >=21 && vitorias <= 50) {
-            nivel = " prata"
-            }
+            let saldo = vitorias - derrotas
+            let nivel = "" ;
 
 
-           else {
+         if (vitorias >=21 && vitorias <= 50){
+               nivel = " prata"
+             }
 
-               if (vitorias >= 51 && vitorias <= 80) {
-                nivel = " ouro"
-            }
 
-           }
+          else{
 
-          return { saldoVitorias: saldo, nivel }
+             if (vitorias >= 51 && vitorias <= 80){
+            nivel = " ouro"}
+                     }
 
-}
-
-let vitorias = 25
-let derrotas = 51
   
-   let resultado = calcularRanking( vitorias, derrotas);
+             return { saldoVitorias: saldo, nivel }
 
-   console.log (`O Heroi tem um saldo de ${resultado.saldoVitorias} vitorias e está no de${resultado.nivel} `);
+                                                     }
+
+
+     let vitorias = 25
+     let derrotas = 51
+  
+     let resultado = calcularRanking( vitorias, derrotas);
+
+        console.log (`O Heroi tem um saldo de ${resultado.saldoVitorias} vitorias e está no de${resultado.nivel} `);
 
