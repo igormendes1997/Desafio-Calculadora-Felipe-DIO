acc582aef10a2e20d0b9e35ca3b3ef328a90ae90
igormendes1997 <igor19_@outlook.com>
2024-03-29 01:34:12 -0300

    foi adicionado a condicional Else e novamente a condicional If e operadores de comparação

--- index.js
+++ index.js
@@ -5,7 +5,19 @@ function calcularRanking(vitorias, derrotas) {
          if (vitorias >=21 && vitorias <= 50) {
             nivel = " prata"
             }
-           
+
+
+           else {
+
+               if (vitorias >= 51 && vitorias <= 80) {
+                nivel = " ouro"
+            }
+
+           }
+
+          return { saldoVitorias: saldo, nivel: nivel };
+
 }
 
-
+let vitorias = 40
+let derrotas = 50
