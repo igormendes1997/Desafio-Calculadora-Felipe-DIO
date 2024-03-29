bc4b5caacd2869bdc66d1225f3552c6c3a578680
igormendes1997 <igor19_@outlook.com>
2024-03-29 01:16:04 -0300

    declarei uma variavel saldo e uma variavel nivel, em seguida adicionei uma Condição e utilizei operadores de comparação

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-function calcularRanking(vitorias, derrotas){
+function calcularRanking(vitorias, derrotas) {
+         let saldo = vitorias - derrotas
+         let nivel = "" ;
 
-    
+         if (vitorias >=21 && vitorias <= 50) {
+            nivel = " prata"
+            }
+           
 }
 
 
